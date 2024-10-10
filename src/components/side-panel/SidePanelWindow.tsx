fc21bde5f9ffe7bdcdfaad766d965ae28abac243
Shane Byrne <130930623+ShaneByrne0963@users.noreply.github.com>
2024-10-10 16:38:55 +0000

    Update layout of side panel to group window components together

--- src/components/side-panel/SidePanelWindow.tsx
+++ src/components/side-panel/SidePanelWindow.tsx
@@ -2,40 +2,56 @@ import { useContext } from "react";
 import SidePanelLabel from "./SidePanelLabel";
 import { ThemeContext } from "../../context";
 
+///////// Configuration
+
+// The size of the window depending on the type
+const windowSizes = {
+  screen: "sm",
+  layers: "lg",
+};
+
+///////// Property Types
+
 type SidePanelWindowProps = {
   windowType: string;
   label?: string;
 };
 
 type WindowComponentProps = {
-  size: string;
+  windowType: string;
 };
+
+///////// Components
 
 function SidePanelWindow(props: SidePanelWindowProps) {
   const color = useContext(ThemeContext);
-  let windowSize = "md";
-  switch (props.windowType) {
-    case "layers":
-      windowSize = "lg";
-      break;
-    default:
-      break;
-  }
-  return (
+  let panelWindowHtml = (
     <>
       {props.label && (
         <SidePanelLabel color={color} text={props.label}></SidePanelLabel>
       )}
-      <WindowComponent size={windowSize}></WindowComponent>
+      <WindowComponent windowType={props.windowType}></WindowComponent>
     </>
   );
+
+  // If the window is accompanied by a label, then wrap the HTML in a div
+  if (props.label) {
+    return (
+      <div id={"window-" + props.windowType + "-container"}>
+        {panelWindowHtml}
+      </div>
+    );
+  }
+  return panelWindowHtml;
 }
 
 function WindowComponent(props: WindowComponentProps) {
+  // Get the size of the window based on the type
+  let windowSize = windowSizes[props.windowType as keyof typeof windowSizes];
   return (
     <div
-      id="select-screen-window"
-      className={"side-panel-window " + props.size}
+      id={"window-" + props.windowType}
+      className={"side-panel-window " + windowSize}
     ></div>
   );
 }
