e2baa27665e8d36bfabe525e73b16fa4b4db37ae
unknown <shanebyrne0963@gmail.com>
2024-10-22 18:49:05 +0100

    Add remaining inputs for custom screen size

--- src/components/window/AddScreenSizeWindow.tsx
+++ src/components/window/AddScreenSizeWindow.tsx
@@ -3,31 +3,69 @@ import { defaultScreenSizes } from "../../context";
 function AddScreenSizeWindow() {
   return (
     <>
-      {defaultScreenSizes.map((item, count) => {
-        return (
-          <div key={"screen-size-" + count}>
-            <label>
-              <input
-                type="radio"
-                name="select-screen-option"
-                className="with-gap"
-                value={count}
-              ></input>
-              <span className="screen-size-radio-text">
-                <div>
-                  <div>{item.name}</div>
-                  <small>
-                    {item.width}px &times; {item.height}px
-                  </small>
-                </div>
-              </span>
-            </label>
-          </div>
-        );
-      })}
+      <fieldset id="default-screen-sizes">
+        <legend>Default Screen Sizes</legend>
+        {defaultScreenSizes.map((item, count) => {
+          return (
+            <div key={"screen-size-" + count}>
+              <label>
+                <input
+                  type="radio"
+                  name="select-screen-option"
+                  className="with-gap"
+                  value={count}
+                />
+                <span className="screen-size-radio-text">
+                  <div>
+                    <div>{item.name}</div>
+                    <small>
+                      {item.width}px &times; {item.height}px
+                    </small>
+                  </div>
+                </span>
+              </label>
+            </div>
+          );
+        })}
+      </fieldset>
 
-      <label htmlFor="screen-size-name">Screen Size Name</label>
-      <input id="screen-size-name" type="text"></input>
+      <fieldset>
+        <legend>Custom Screen Size</legend>
+        <div id="add-custom-screen-size">
+          <label>
+            <input
+              type="radio"
+              name="select-screen-option"
+              className="with-gap"
+              value="Custom"
+            />
+            <span>Use Custom Screen Size</span>
+          </label>
+
+          <div id="custom-screen-size-inputs">
+            <div>
+              <label htmlFor="screen-size-name">Screen Size Name</label>
+              <input
+                id="screen-size-name"
+                type="text"
+                placeholder="Name"
+                required
+              />
+            </div>
+            <label>Dimensions</label>
+            <div id="dimensions-container">
+              <div>
+                <input id="dimension-x" type="number" placeholder="1920" />
+              </div>
+              <div className="multiplier">&times;</div>
+              <div>
+                <input id="dimension-y" type="number" placeholder="1080" />
+              </div>
+              <div id="screen-size-units">Pixels</div>
+            </div>
+          </div>
+        </div>
+      </fieldset>
     </>
   );
 }
