6eb45f8527ecfecfb32f5cc4af1e37af29a1e9e7
unknown <shanebyrne0963@gmail.com>
2024-10-25 13:03:38 +0100

    Add options menu icon beside page name dropdown

--- src/components/side-panel/PageName.tsx
+++ src/components/side-panel/PageName.tsx
@@ -1,8 +1,13 @@
 function PageName() {
   return (
-    <div id="selected-page">
-      <div className="side-panel-label">Page 1</div>
-      <i className="small material-icons">arrow_drop_down</i>
+    <div id="page-select">
+      <div id="selected-page">
+        <div className="side-panel-label">Index</div>
+        <i className="small material-icons">arrow_drop_down</i>
+      </div>
+      <a role="button" className="max-height-square">
+        <i className="material-icons">menu</i>
+      </a>
     </div>
   );
 }
