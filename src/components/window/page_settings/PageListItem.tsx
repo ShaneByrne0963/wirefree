e9fe28978d97df54a14b29f79ef24691b770bc83
unknown <shanebyrne0963@gmail.com>
2024-11-13 13:39:58 +0000

    Add edit and delete icons to page list item

--- src/components/window/page_settings/PageListItem.tsx
+++ src/components/window/page_settings/PageListItem.tsx
@@ -3,7 +3,13 @@ interface PageListItemProps {
 }
 
 function PageListItem(props: PageListItemProps) {
-  return <div>{props.name}</div>;
+  return (
+    <div className="page-item">
+      <div className="page-name">{props.name}</div>
+      <button className="plain max-height-square material-icons">create</button>
+      <button className="plain max-height-square material-icons">delete</button>
+    </div>
+  );
 }
 
 export default PageListItem;
