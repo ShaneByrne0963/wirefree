71b21dd3b778ec516de7d7debc7a3b1a113feb46
unknown <shanebyrne0963@gmail.com>
2024-11-20 18:03:54 +0000

    Add input for editing page name in page settings list item

--- src/components/window/page_settings/PageListItem.tsx
+++ src/components/window/page_settings/PageListItem.tsx
@@ -1,17 +1,53 @@
+import { useState } from "react";
+
 interface PageListItemProps {
   name: string;
   canDelete: boolean;
 }
 
 function PageListItem(props: PageListItemProps) {
+  const [editMode, setEditMode] = useState(false);
+  const [val, setVal] = useState(props.name);
+
   return (
     <div className="page-item">
-      <div className="page-name">{props.name}</div>
-      <button className="plain max-height-square material-icons">create</button>
-      {props.canDelete && (
-        <button className="plain max-height-square material-icons">
-          delete
-        </button>
+      {editMode ? (
+        <>
+          <input
+            type="text"
+            value={val}
+            className="edit-page-item browser-default"
+            autoFocus
+            onChange={(event) => setVal(event.target.value)}
+          />
+          <button className="plain max-height-square material-icons">
+            check
+          </button>
+          <button
+            className="plain max-height-square material-icons"
+            onClick={() => {
+              setEditMode(false);
+              setVal(props.name);
+            }}
+          >
+            close
+          </button>
+        </>
+      ) : (
+        <>
+          <div className="page-name">{props.name}</div>
+          <button
+            className="plain max-height-square material-icons"
+            onClick={() => setEditMode(true)}
+          >
+            create
+          </button>
+          {props.canDelete && (
+            <button className="plain max-height-square material-icons">
+              delete
+            </button>
+          )}
+        </>
       )}
     </div>
   );
