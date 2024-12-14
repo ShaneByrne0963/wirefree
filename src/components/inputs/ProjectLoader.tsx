b5f46b2f4644bbfebc94753aa547aa37f8a315a7
unknown <shanebyrne0963@gmail.com>
2024-12-14 11:01:01 +0000

    Add functionality to read file input on project load

--- src/components/inputs/ProjectLoader.tsx
+++ src/components/inputs/ProjectLoader.tsx
@@ -1,5 +1,31 @@
+import { useRef } from "react";
+
 function ProjectLoader() {
-  return <input type="file" id="project-loader" accept=".json" />;
+  const loaderRef = useRef<HTMLInputElement | null>(null);
+
+  function handleInput() {
+    if (!loaderRef.current) return;
+    const files = loaderRef.current.files;
+    if (!files) return;
+
+    // Read the data from the input
+    const reader = new FileReader();
+    reader.readAsText(files[0], "UTF-8");
+    reader.onload = (event) => {
+      if (!event.target) return;
+      console.log(event.target.result);
+    };
+  }
+
+  return (
+    <input
+      type="file"
+      id="project-loader"
+      accept=".json"
+      onChange={handleInput}
+      ref={loaderRef}
+    />
+  );
 }
 
 export default ProjectLoader;
